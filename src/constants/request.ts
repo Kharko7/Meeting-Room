da6eb7105f306856365c15a35ecbf5208c40dcf0
Kharko7 <hanley77@ukr.net>
2022-12-28 17:19:57 +0200

    removed old ownBookings

--- src/constants/request.ts
+++ src/constants/request.ts
@@ -1,7 +1,6 @@
 // const baseURL = 'https://coursep.herokuapp.com/';
 export const baseURL = 'https://cors-anywhere.herokuapp.com/https://coursep.herokuapp.com/';
 
-
 export const urls = {
     login: '/signin',
     register: '/signup',
